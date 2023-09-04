0fda74e40dd0095602be59825effb140c028281b
changgun-park <pcg0537@gmail.com>
2023-09-04 17:46:53 +0900

    feat: TaskRow UI 컴포넌트

--- src/entities/task/ui/task-row/index.tsx
+++ src/entities/task/ui/task-row/index.tsx
@@ -1,10 +1,30 @@
+import { styled } from "styled-components";
 import { Row } from "antd";
+import { Link } from "react-router-dom";
 
 interface TaskRowProps {
-  data: string;
+  data: import("shared/api").Task;
   titleHref: string;
+  before?: React.ReactNode;
 }
 
-export const TaskRow = ({ data, titleHref }: TaskRowProps) => {
-  return <Row></Row>;
+export const TaskRow = ({ data, titleHref, before }: TaskRowProps) => {
+  const title = titleHref ? (
+    <Link to={titleHref}>{data.title}</Link>
+  ) : (
+    data.title
+  );
+
+  return (
+    <StyledRow $completed={data.completed}>
+      {before}
+      {title}
+    </StyledRow>
+  );
 };
+
+const StyledRow = styled(Row)<{ $completed?: boolean }>`
+  text-decoration: ${({ $completed }) =>
+    $completed ? "line-through" : "none"};
+  opacity: ${({ $completed }) => ($completed ? 0.5 : 1)};
+`;
